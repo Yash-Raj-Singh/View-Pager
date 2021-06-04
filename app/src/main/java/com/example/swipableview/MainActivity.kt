85a518b2aa7f9f001669990e288db8808176744b
Yash Raj Singh <yashrajsingh807@gmail.com>
2021-06-04 13:47:49 +0530

    Adding TabLayout to switch between views easily.

--- app/src/main/java/com/example/swipableview/MainActivity.kt
+++ app/src/main/java/com/example/swipableview/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.swipableview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,5 +29,24 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ViewPagerAdapter(images)
         viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+            tab.text = "Tab ${position+1}"
+        }.attach()
+
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Selected ${tab?.text}",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "UnSelected ${tab?.text}",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "ReSelected ${tab?.text}",Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
